6d4c2d393b38f48b2ac22b658163ea68d8fd2291
Allyson-Santana <santana.allyson459@gmail.com>
2022-10-08 17:17:14 -0300

    Sistema de download de video do YouTube

--- public/home/script.js
+++ public/home/script.js
@@ -1,1 +1,22 @@
-console.log("OKO")
+"use strict";
+
+function handlerSubmit() {
+  let url_youtube = String(document.querySelector('#url-youtube').value);
+  let url = url_youtube.trim();
+
+  if (url_youtube === undefined || url_youtube === null || url === '') {
+    console.log("Error: URL YouTube is required");
+    return;
+  }
+
+  let redirect = `${window.location.origin}/download?url=${url}`;
+  
+  window.location.replace(redirect);
+  
+  let msg =  document.querySelector("#message");
+
+  msg.innerHTML = "Agurde o processamento do video...";
+
+  setTimeout(() => msg.innerHTML = "", 4000);
+
+}
